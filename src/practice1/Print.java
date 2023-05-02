59d41fd9110969796fb021a5d079744202c26a5f
Andrew Balint <Andrew.balint@hotmail.com>
2023-05-02 12:59:57 -0700

    Initial commit

---
+++ src/practice1/Print.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("git practice");
+        System.out.println("adding changes");
+    }
+}
---
+++ src/practice2/Random.java
@@ -0,0 +1,7 @@
+package practice2;
+
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("Blank");
+    }
+}
