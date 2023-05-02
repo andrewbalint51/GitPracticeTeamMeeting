56872bed4fdc29ed8b84d16212c46c883cc0d359
Andrew Balint <Andrew.balint@hotmail.com>
2023-05-02 13:04:16 -0700

    Changes added

--- src/practice2/Random.java
+++ src/practice2/Random.java
@@ -3,5 +3,10 @@ package practice2;
 public class Random {
     public static void main(String[] args) {
         System.out.println("Blank");
+
+        System.out.println("Changes");
+
+        System.out.println("Stuff");
+
     }
 }
